0e20a2514773c4e86ff822aa6dba774a59727067
bohort-32 <108988458+bohort-32@users.noreply.github.com>
2022-07-16 17:54:31 +0200

    Ajout chargement des données Utilisateur.

--- Classes/Utilisateur.py
+++ Classes/Utilisateur.py
@@ -2,19 +2,35 @@ import json
 
 # Gère les interfaces utilisateur
 class Utilisateur:
-    def __init__(self, nom, clef_maitre, win_usr=''):
+    def __init__(self, nom='', clef_maitre='', win_usr=''):
         self.nom = nom
         self.clef_maitre = clef_maitre
         self.win_usr = win_usr
-        self.sauvegarder()
+        if (self.nom and clef_maitre and win_usr) != '':
+            self.sauvegarder()
 
     def sauvegarder(self):
         # Définie les données à sauvegarder
-        sav_user = {"name": self.nom, "ClefMaitre": self.clef_maitre, "WinUser":self.win_usr}
+        sav_user = {"nom": self.nom, "ClefMaitre": self.clef_maitre, "WinUser":self.win_usr}
         # Ouvre le fichier
         with open(f'./DATA/{self.win_usr}/utilisateur.json', 'w') as outfile:
             # Ecris
-            json.dump(json.dumps(sav_user), outfile)
+            json.dump(sav_user, outfile)
+
+
+    def charger_donnees_utilisateur(self, nom_fichier):
+        # Ouverture du fichier de chargement
+        with open(nom_fichier, 'r') as f:
+            données_user = json.load(f)
+        
+        # Extraction des données
+        self.nom = données_user['nom']
+        self.clef_maitre = données_user['ClefMaitre']
+        self.win_usr = données_user['WinUser']
+
+        # Retour des données
+        return self
+        
 
     # Change et sauvegarde le nom
     def set_nom(self, nom_nv):
--- Modules/Demarrage.py
+++ Modules/Demarrage.py
@@ -3,20 +3,25 @@ from Modules.Initialisation import *
 from Classes.UserInterface import *
 
 
-def verifierInit(USER_NAME, USERInterface):
-    # Chemin des données utilisateurs
-    chemin_DATA = f'./DATA'
-    # Chemin des données de l'utilisateur courant
-    chemin_user = f'{chemin_DATA}/{USER_NAME}'
+def verifierInit(USER_NAME, USERInterface, chemin_DATA, chemin_user):
     # == Vérification présence des dossier
     data_exist = os.path.exists(chemin_DATA)
     chemin_user_exist = os.path.exists(chemin_user)
 
     if not data_exist or not chemin_user_exist:
         initialiser_dossier(USER_NAME, USERInterface, chemin_DATA, chemin_user, data_exist, chemin_user_exist)
+    
 
 
 def demarrerScript():
+    # == Initialisation des variables
     USER_NAME = os.environ.get( "USERNAME" )
     USERInterface = UserInterface()
-    verifierInit(USER_NAME, USERInterface)
+    # Chemin des données utilisateurs
+    chemin_DATA = f'./DATA'
+    # Chemin des données de l'utilisateur courant
+    chemin_user = f'{chemin_DATA}/{USER_NAME}'
+    chemin_fic_user = f'{chemin_user}/utilisateur.json'
+    # Vérifie qu'un utilisateur existe ou le crée
+    verifierInit(USER_NAME, USERInterface, chemin_DATA, chemin_user)
+    user = Utilisateur().charger_donnees_utilisateur(chemin_fic_user)
