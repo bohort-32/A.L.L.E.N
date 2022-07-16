f8c4aae3e05764ea7bbe8be97261e1cabad7b295
bohort-32 <108988458+bohort-32@users.noreply.github.com>
2022-07-16 17:24:58 +0200

    Ajout setters Utilisateur.

--- Classes/Utilisateur.py
+++ Classes/Utilisateur.py
@@ -12,3 +12,18 @@ class Utilisateur:
         sav_user = {"name": self.nom, "ClefMaitre": self.clef_maitre, "WinUser":self.win_usr}
         with open(f'./DATA/{self.win_usr}/utilisateur.json', 'w') as outfile:
             json.dump(json.dumps(sav_user), outfile)
+
+
+    def set_nom(self, nom_nv):
+        self.nom = nom_nv
+        self.sauvegarder()
+
+
+    def set_clef_maitre(self, clef_maitre_nv):
+        self.clef_maitre_nv = clef_maitre_nv
+        self.sauvegarder()
+
+
+    def set_win_usr(self, win_usr_nv):
+        self.win_usr = win_usr_nv
+        self.sauvegarder()    
