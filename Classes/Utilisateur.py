050c423ab3a5d126d1b1df2a53fa2b83e65e4c3f
bohort-32 <108988458+bohort-32@users.noreply.github.com>
2022-07-16 17:26:56 +0200

    Ajout de commentaires.

--- Classes/Utilisateur.py
+++ Classes/Utilisateur.py
@@ -9,21 +9,24 @@ class Utilisateur:
         self.sauvegarder()
 
     def sauvegarder(self):
+        # Définie les données à sauvegarder
         sav_user = {"name": self.nom, "ClefMaitre": self.clef_maitre, "WinUser":self.win_usr}
+        # Ouvre le fichier
         with open(f'./DATA/{self.win_usr}/utilisateur.json', 'w') as outfile:
+            # Ecris
             json.dump(json.dumps(sav_user), outfile)
 
-
+    # Change et sauvegarde le nom
     def set_nom(self, nom_nv):
         self.nom = nom_nv
         self.sauvegarder()
 
-
+    # Change et sauvegarde la clef
     def set_clef_maitre(self, clef_maitre_nv):
         self.clef_maitre_nv = clef_maitre_nv
         self.sauvegarder()
 
-
+    # Change et sauvegarde l'id windows
     def set_win_usr(self, win_usr_nv):
         self.win_usr = win_usr_nv
         self.sauvegarder()    
