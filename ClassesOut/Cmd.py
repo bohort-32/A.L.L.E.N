35321c9cc9d3f04963c18731bb6cc9a0df12aee4
bohort-32 <108988458+bohort-32@users.noreply.github.com>
2022-07-23 18:13:01 +0200

    Commentaire du module extérieur CMD.

--- ClassesOut/Cmd.py
+++ ClassesOut/Cmd.py
@@ -7,19 +7,25 @@ from ModulesCoeur.Comprendre import *
 # class inside the module
 class Cmd:
     def Comprendre(USERInterface, User, requete):
+        # Liste des mots clefs système
         LISTE_SYS = ['CMD', 'cmd', 'Cmd']
+        # Indique si la requete est présente
         if rechercher_mot(requete, LISTE_SYS):
+            # Formulation de la commande
             requete = requete.split(' ')
             if len(requete) > 1:
                 commande = ''
                 for cmd_arg in requete:
                     if cmd_arg != requete[0]:  
                         commande += ' '+cmd_arg
+                # Execution de la commande
                 res = os.system(commande)
-
+                # Commande avec succès
                 if res == 0:
                     USERInterface.informer_utilisateur('Commande exécutée')
+                # Erreur arrivée
                 else :
-                    USERInterface.informer_utilisateur('Commande inconnue')
+                    USERInterface.informer_utilisateur('Erreur commande')
+            # Pas de commande donnée
             else:
                 USERInterface.informer_utilisateur('Pas de commande donnée')
